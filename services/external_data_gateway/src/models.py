86c60d75f7b470f64413eb5940e7c1d7a35b6968
Aiden Gindin <aiden@thegindins.com>
2025-01-29 16:02:05 -0500

    Remove Activity model from external data gateway models

--- services/external_data_gateway/src/models.py
+++ services/external_data_gateway/src/models.py
@@ -12,33 +12,6 @@ class SyncStatus(str, Enum):
     FAILED = "failed"
 
 
-
-
-class Activity(BaseModel):
-    id: str
-    start_date: datetime
-    name: str
-    description: Optional[str] = None
-    sport_type: str
-    duration: float
-    total_elevation_gain: Optional[float] = None
-    distance: Optional[float] = None
-    average_speed: Optional[float] = None
-    average_heartrate: Optional[int] = None
-    average_cadence: Optional[float] = None
-    average_power: Optional[float] = None
-    calories: Optional[int] = None
-    average_lr_balance: Optional[float] = None
-    gear: Optional[Gear] = None
-    average_gap: Optional[float] = None
-    perceived_exertion: Optional[int] = None
-    polarization_index: Optional[float] = None
-    decoupling: Optional[float] = None
-    carbs_ingested: Optional[float] = None
-    normalized_power: Optional[int] = None
-    training_load: Optional[int] = None
-
-
 class SyncRequest(BaseModel):
     user_id: str
     start_date: Optional[datetime] = None
